97eda98c923320f84fe51af2106b9429e5ee487d
sai vardhini <saivardhinirouthu5566@gmail.com>
2024-07-06 15:54:04 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let menuIcon=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
 let sections=document.querySelectorAll('section');
@@ -25,4 +26,33 @@ window.onscroll = () => {
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+=======
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nava');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top= window.scrollY;
+        let offset= sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id =sec.getAttribute;
+        
+        if(top > offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +' ]').classList.add('active')
+            })
+        }
+    })
+}
+
+
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+>>>>>>> 16a9041 (Initial commit)
 }
